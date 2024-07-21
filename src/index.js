b66d2f365828a2b5d902b8dd99ccb1524efe10c8
MsSuraj001 <milukumar225@gmail.com>
2024-07-21 13:57:34 +0530

    added the some code

--- src/index.js
+++ src/index.js
@@ -17,8 +17,9 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     credentials : true,
+    // optionsSuccessStatus: 200,
 }));
 app.use(cookieParser());
 app.use(express.json());
